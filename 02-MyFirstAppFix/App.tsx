c1dc280164998d5aa57cf43d973ff97a733e111f
Jhonatan Piero Abarca Murillo <jpieroabarcam@gmail.com>
2025-07-26 18:36:48 -0500

    fix latest folder

---
+++ 02-MyFirstAppFix/App.tsx
@@ -0,0 +1,16 @@
+
+import { SafeAreaView } from 'react-native'
+import { HelloWorldScreen } from './src/presentation/screens/HelloWorldScreen'
+import { CounterScreen } from './src/presentation/screens/CounterScreen'
+import { PaperProvider } from 'react-native-paper'
+
+export const App = () => {
+  return (
+    <PaperProvider>
+      <SafeAreaView style={{ flex: 1 }}>
+        {/* <HelloWorldScreen name='Piero Abarca'/> */}
+        <CounterScreen />
+      </SafeAreaView>
+    </PaperProvider>
+  )
+}
---
+++ 02-MyFirstAppFix/src/presentation/screens/CounterScreen.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react"
+import { Text, View, StyleSheet } from "react-native"
+import { PrimaryButton } from "./components"
+
+export const CounterScreen = () => {
+
+  const [count, setCount] = useState(10)
+
+  return (
+    <View style={styles.container}>
+        <Text style={styles.title}>{ count }</Text>
+
+        <PrimaryButton 
+            label='Incrementar'
+            onPress={ () => setCount( count +1)}
+            onLongPress={ () => setCount(0)}
+        />
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    title: {
+        fontSize:80,
+        color: 'black',
+        fontWeight: '300'
+    },
+})
