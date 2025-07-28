d26f42e8df9db7ca54fed158872a2b6721a45254
Jhonatan Piero Abarca Murillo <jpieroabarcam@gmail.com>
2025-07-27 22:16:52 -0500

    review for styles in react native

--- 02-MyFirstApp/App.tsx
+++ 02-MyFirstApp/App.tsx
@@ -5,6 +5,8 @@ import { CounterScreen } from './src/presentation/screens/CounterScreen'
 import { PaperProvider } from 'react-native-paper'
 import { CounterM3Screen } from './src/presentation/screens/CounterM3Screen'
 import Icons from '@react-native-vector-icons/ionicons'
+import { BoxObjectModelScreen } from './src/presentation/screens/BoxObjectModelScreen'
+import { DimensionScreen } from './src/presentation/screens/DimensionScreen'
 
 export const App = () => {
   return (
@@ -16,7 +18,9 @@ export const App = () => {
       <SafeAreaView style={{ flex: 1 }}>
         {/* <HelloWorldScreen name='Piero Abarca'/> */}
         {/* <CounterScreen /> */}
-        <CounterM3Screen />
+        {/* <CounterM3Screen /> */}
+        {/* <BoxObjectModelScreen /> */}
+        <DimensionScreen />
       </SafeAreaView>
     </PaperProvider>
   )
