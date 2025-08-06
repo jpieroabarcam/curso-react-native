24b05bac11763e5059707f8796600adb77ed6959
Jhonatan Piero Abarca Murillo <jpieroabarcam@gmail.com>
2025-08-05 21:29:25 -0500

    adding side menu navigator drawer

--- 04-NavigationApp/src/App.tsx
+++ 04-NavigationApp/src/App.tsx
@@ -1,10 +1,12 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { StackNavigator } from './presentation/routes/StackNavigator'
+import { SideMenuNavigator } from './presentation/routes/SideMenuNavigator'
 
 export const App = () => {
   return (
     <NavigationContainer>
-      <StackNavigator />
+      {/* <StackNavigator /> */}
+      <SideMenuNavigator />
     </NavigationContainer>
   )
 }
