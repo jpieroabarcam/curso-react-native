508feecf77d14b238786cf74606fe6cc74ef74b7
Jhonatan Piero Abarca Murillo <jpieroabarcam@gmail.com>
2025-09-30 20:43:24 -0500

    adding google map and location

---
+++ 09-mapsApp/src/actions/location/location.ts
@@ -0,0 +1,18 @@
+import Geolocation from '@react-native-community/geolocation';
+import { Location } from '../../infraestructure/interfaces/location';
+
+export const getCurrentLocation = async():Promise<Location> => {  
+    return new Promise((resolve, reject) => {
+        Geolocation.getCurrentPosition( info => {
+            resolve({
+                latitude: info.coords.latitude,
+                longitude: info.coords.longitude
+            })    
+        }, error => {
+            console.log(`Can't get location`);
+            reject(error)
+        }), { enableHighAccuracy: true };
+        
+
+    });
+}
