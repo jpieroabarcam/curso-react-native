7844f25388b7e9d9c084ce16df42f863ed319133
Jhonatan Piero Abarca Murillo <jpieroabarcam@gmail.com>
2025-10-02 19:07:00 -0500

    new backend and add final project productApp with amination

---
+++ 11-productsApp/src/presentation/navigation/StackNavigator.tsx
@@ -0,0 +1,43 @@
+import { createStackNavigator, StackCardStyleInterpolator } from '@react-navigation/stack';
+import { HomeScreen } from '../screens/home/HomeScreen';
+import { LoginScreen } from '../screens/auth/LoginScreen';
+import { RegisterScreen } from '../screens/auth/RegisterScreen';
+import { LoadingScreen } from '../screens/loading/LoadingScreen';
+import { ProductScreen } from '../screens/product/ProductScreen';
+
+
+export type RootStackParams = {
+    LoadingScreen: undefined;
+    LoginScreen: undefined;
+    HomeScreen: undefined;
+    RegisterScreen: undefined;
+    ProductScreen: { productId: string };
+}
+
+const Stack = createStackNavigator<RootStackParams>();
+
+const fadeAnimation: StackCardStyleInterpolator = ({ current }) => {
+    return {
+        cardStyle: {
+            opacity: current.progress
+        }
+    }
+}
+
+export const StackNavigator =() => {
+  return (
+    <Stack.Navigator 
+        initialRouteName='LoginScreen'
+        screenOptions={{ 
+            headerShown: false, 
+            // cardStyleInterpolator: fadeAnimation
+        }}
+    >
+      <Stack.Screen options={{cardStyleInterpolator: fadeAnimation}} name="LoadingScreen" component={LoadingScreen} />      
+      <Stack.Screen options={{cardStyleInterpolator: fadeAnimation}} name="LoginScreen" component={LoginScreen} />      
+      <Stack.Screen options={{cardStyleInterpolator: fadeAnimation}} name="HomeScreen" component={HomeScreen} />      
+      <Stack.Screen options={{cardStyleInterpolator: fadeAnimation}} name="RegisterScreen" component={RegisterScreen} />      
+      <Stack.Screen name="ProductScreen" component={ProductScreen} />      
+    </Stack.Navigator>
+  );
+}
